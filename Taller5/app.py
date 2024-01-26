8967b7ec280b3727bf89db8e16f79d1b6725237b
Jose_Mejia <enriquemejiagamarra@gmail.com>
2024-01-25 23:50:42 -0500

    add solution for Taller5

--- Taller5/app.py
+++ Taller5/app.py
@@ -8,6 +8,9 @@ input_text = st.text_input('Ingrese algún texto')
 
 # Crear un botón
 if st.button('Mostrar Imagen'):
+    ###############
+    ####LOGICA#####
+    ###############
     # Si se hace clic en el botón, mostrar la imagen
     st.image('https://pub-3626123a908346a7a8be8d9295f44e26.r2.dev/generations/0-430cb9b2-bc0a-4a23-8ae3-b7290fecea1a.png',
              caption='Imagen Mostrada')
