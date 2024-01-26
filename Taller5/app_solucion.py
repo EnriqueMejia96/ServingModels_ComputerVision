6cbebff987b9fbca78fe34b39edfc009276b8152
Jose_Mejia <enriquemejiagamarra@gmail.com>
2024-01-25 23:52:41 -0500

    add solution for Taller5

--- Taller5/app_solucion.py
+++ Taller5/app_solucion.py
@@ -13,7 +13,7 @@ if st.button('Mostrar Imagen'):
     url =  "https://stablediffusionapi.com/api/v4/dreambooth"  
 
     payload = json.dumps({  
-    "key":  "mFYRN0yRcpWXfUuIupvQqThCwsshHt70oQgYNVOsybtmynJJlZcHLYZbub4R",  
+    "key":  "TOKEN_ID",  
     "model_id":  "juggernaut-xl-v5",  
     "prompt":  input_text,  
     "negative_prompt":  "painting, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime",  
